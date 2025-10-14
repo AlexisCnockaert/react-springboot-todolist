bb1eeb515949dd79543a90691c22f72574ba1616
Cnockaert Alexis <alexis.cnockaert.etu@b04p3.fil.univ-lille.fr>
2025-10-14 10:15:32 +0200

    update front a bit'

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,11 +10,13 @@ return (
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=BBH+Sans+Hegarty&display=swap" rel="stylesheet"/>
-            <h1 className="neon-title">Your innovative Todo List</h1>
+            <h1 className="neon-title">To Done</h1>
             <p className="intro-desc">Let's make productivity glow</p>
             <p className="intro-text">
-                Manage your tasks with a <span className="glow">touch of style</span> —
-                fast, fluid, and a little futuristic.
+                Manage your tasks with a <span className="glow">touch of style </span>
+                - all in a futuristic and optimized way
+                Let AI guide you through each step of tour tasks
+
             </p>
 
             <div className="App">
