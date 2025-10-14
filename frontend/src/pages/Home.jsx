e5bfe91e5a7bd1c995c365d71336047fb3d7a903
Cnockaert Alexis <alexis.cnockaert.etu@b04p3.fil.univ-lille.fr>
2025-10-14 14:39:58 +0200

    description update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,11 +11,11 @@ return (
             <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=BBH+Sans+Hegarty&display=swap" rel="stylesheet"/>
             <h1 className="neon-title">To Done</h1>
-            <p className="intro-desc">Let's make productivity glow</p>
+            <p className="intro-desc">Be productive, efficient, organized</p>
             <p className="intro-text">
                 Manage your tasks with a <span className="glow">touch of style </span>
                 - all in a futuristic and optimized way
-                Let AI guide you through each step of tour tasks
+                Let AI guide you through each step of your tasks
 
             </p>
 
